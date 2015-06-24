6bc3093b9bce0bdb0b0bb282cdb921a185f695a5
Jerome Velociter <jerome@velociter.fr>
2015-06-24 11:24:41 +0200

    Fix phpDoc

--- public/wp-content/themes/project-theme/lib/Utils/Utils.php
+++ public/wp-content/themes/project-theme/lib/Utils/Utils.php
@@ -29,7 +29,7 @@ class Utils
 
     /**
      * Handles meta data and save it in the database.
-     * @param  Integer $fields The ID of the post.
+     * @param  Integer $post_id The ID of the post.
      * @param  Array   $fields An array containing the names of the fields.
      */
     static public function handleMetaData($post_id, $fields)
