ed4e768aec2560ac6e0c62c8aaf7960b8f094de9
Johann Pardanaud <pardanaud.j@gmail.com>
2015-07-30 14:40:45 +0200

    Remove custom fields from the boilerplate, make them standalone in a Wordpress plugin.
    See: https://github.com/46cl/wp-custom-fields

--- public/wp-content/themes/project-theme/functions.php
+++ public/wp-content/themes/project-theme/functions.php
@@ -8,20 +8,6 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 // Load the libraries
 require_once __DIR__ . '/lib/autoload.php';
 
-foreach (glob(__DIR__ . '/lib/*/') as $libraryPath) {
-    $library = basename($libraryPath);
-    $file = __DIR__  . '/lib/' . $library . '/' . $library . '.php';
-
-    if (file_exists($file)) {
-        require_once $file;
-
-        $class = new ReflectionClass($library);
-        if ($class->implementsInterface('LoaderInterface')) {
-            call_user_func($library . '::load');
-        }
-    }
-}
-
 // Load the configuration
 foreach (glob(__DIR__ .'/config{/,/*/}*.php', GLOB_BRACE) as $filename) {
     require_once $filename;
